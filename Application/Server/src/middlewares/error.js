477c87b7f9fff06165e50f96d8f71d7fcc9ec660
Radostin Kostadinov <dev@rkostadinov.com>
2024-12-30 00:50:03 +0200

    Small refactoring, code explanation

--- Application/Server/src/middlewares/error.js
+++ Application/Server/src/middlewares/error.js
@@ -4,17 +4,38 @@ import { config, logger } from '../config/index.js';
 import BaseError from '../utils/error_handler/error_types/BaseError.js';
 import ApiError from '../utils/error_handler/error_types/ApiError.js';
 
+/**
+ * Converts any error to an instance of ApiError if it is not already a BaseError.
+ *
+ * @param {Error} err - The error object to be converted.
+ * @param {Object} req - The express request object.
+ * @param {Object} res - The express response object.
+ * @param {Function} next - The next middleware function in the stack.
+ */
 export const errorConverter = (err, req, res, next) => {
   let error = err;
+  // Check if the error is not an instance of BaseError
   if (!(error instanceof BaseError)) {
+    // Determine the status code based on the error type or default to internal server error
     const statusCode =
       error.statusCode || error instanceof mongooseError ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
+    // Get the error message or use the default message for the status code
     const message = error.message || httpStatus[statusCode];
+    // Create a new ApiError instance
     error = new ApiError(statusCode, message, false, err.stack);
   }
+  // Pass the error to the next middleware
   next(error);
 };
 
+/**
+ * Error handler middleware.
+ *
+ * @param {Error} err - The error object passed to the middleware.
+ * @param {Object} req - The express request object.
+ * @param {Object} res - The express response object.
+ * @param {Function} next - The next middleware function in the stack.
+ */
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
   let { httpCode, message } = err;
